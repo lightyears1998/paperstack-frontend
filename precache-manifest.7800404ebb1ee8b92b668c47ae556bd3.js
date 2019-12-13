0ad2cd0f9999e0f6225fda637a91ac5f148c9d63
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-13 13:36:09 +0000

    Deploy lightyears1998/paperstack-frontend to github.com/lightyears1998/paperstack-frontend.git:gh-pages

---
+++ precache-manifest.7800404ebb1ee8b92b668c47ae556bd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4023f1237f65835ea313580773dea8",
+    "url": "https://lightyears1998.github.io/paperstack-frontend/index.html"
+  },
+  {
+    "revision": "3049915321448a94297f",
+    "url": "https://lightyears1998.github.io/paperstack-frontend/static/css/2.cc82b09a.chunk.css"
+  },
+  {
+    "revision": "cbe1b214664fb56b0f48",
+    "url": "https://lightyears1998.github.io/paperstack-frontend/static/css/main.6128ed41.chunk.css"
+  },
+  {
+    "revision": "3049915321448a94297f",
+    "url": "https://lightyears1998.github.io/paperstack-frontend/static/js/2.7530d956.chunk.js"
+  },
+  {
+    "revision": "cbe1b214664fb56b0f48",
+    "url": "https://lightyears1998.github.io/paperstack-frontend/static/js/main.e6d90e30.chunk.js"
+  },
+  {
+    "revision": "53f9d24989ef7cce240a",
+    "url": "https://lightyears1998.github.io/paperstack-frontend/static/js/runtime-main.cd2550ec.js"
+  },
+  {
+    "revision": "8b24e9b7d357e648529e0dcf9d6fbe90",
+    "url": "https://lightyears1998.github.io/paperstack-frontend/static/media/gzhu-logo.8b24e9b7.png"
+  },
+  {
+    "revision": "403819d64331a96bb68b1757c70ebdbe",
+    "url": "https://lightyears1998.github.io/paperstack-frontend/static/media/home-background.403819d6.jpg"
+  }
+]);
