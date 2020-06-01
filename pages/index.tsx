acdb20c86b4c307b8972a8ef1cf644d849421e72
lightyears1998 <lightyears1998@hotmail.com>
2020-06-01 14:40:30 +0800

    chore:  迁移 ESLint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import { NextPage } from 'next';
+import { NextPage } from "next";
 
 const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-  <h1>Hello world! - user agent: {userAgent}</h1>
+    <h1>Hello world! - user agent: {userAgent}</h1>
 );
 
-Home.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
-  return { userAgent };
+Home.getInitialProps = ({ req }) => {
+    const userAgent = req ? req.headers["user-agent"] || "" : navigator.userAgent;
+    return { userAgent };
 };
 
 export default Home;
